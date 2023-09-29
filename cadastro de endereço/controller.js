34d1b2873919faf1607994a50a64849abbff1c33
leonardo oliveira <azideiamano20209@gmail.com>
2023-09-29 12:08:09 -0300

    update

--- cadastro de endereço/controller.js
+++ cadastro de endereço/controller.js
@@ -1,15 +1,29 @@
 'use strict'; // moodo restrito 
 // consumindo API e CEP  do viaCep
 // http://viacep.com.br/
+// Verifica se Cep é valido
 
-//função para limpar o formulário
-const  limparFormulario = () => {
-    document.getElementById('rua').value = '';
-    document.getElementById('bairro').value = '';
-    document.getElementById('cidade').value = '';
-    document.getElementById('estado').value = '';
-  }
+const eNumero = (numero) => /^[0-9]+$/.test(numero); //testa número informado com expresão regular
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);// Verifica tamanho do cep digite e executa função de validação do cep eNumero
 
-  //verificado se cep é valido
-  const eNumero = (numero) => /^[0-9]+$/.test(numero); //
-  const cepValido = (cep) => cep.lengh == 8 && eNumero(cep); //
+//função para preencher formulario
+const preencherFormulario = (endereco) => {
+    document.getElementById('rua').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = enderco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+}
+
+//Consumo da API da ViaCEp
+const pesquisarCep = async() => {
+    limparFormulario();
+    const url = `http://viacep.com.br/ws/${cep.value}/json/`;
+}
+if(cepValido(value)){
+    const dados = await fetch(url); //aguardar
+    const adrres = await dados.json();
+
+    if(adrres.hasOwnProperty('erro')){
+
+    }
+}
